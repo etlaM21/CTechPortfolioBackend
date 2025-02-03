7d7533097b98fa1ac2591b653d9134ae6a5bb7b2
Malte Hillebrand <maltehillebrand96@gmail.com>
2025-02-03 18:28:14 +0100

    made about page pretty

--- portfolio_v5_2/src/components/About.jsx
+++ portfolio_v5_2/src/components/About.jsx
@@ -1,11 +1,15 @@
 
 const About = () => {
-    return <div id="about">
-        <h1>Hi, I am Sunny!</h1>
-        <p>I am an artist and programmer who creates digital art and interactive installations. I use code to create art that explores the relationship between humans and technology. My work often deals with themes of surveillance, control, and power. I am interested in how technology can be used to create new ways of seeing and experiencing the world. I have a background in computer science and art, and I use this knowledge to create art that is both visually stunning and intellectually stimulating.</p>
-        <p>I studied Creative Technologies at the Filmuniversity Babelsberg. The rest is history!</p>
-        <p>Get in Touch: email </p>
-    </div>;
+    return (
+        <main>
+            <div className="about">
+                <h1>Hi, I am Sunny! ✌️</h1>
+                <p>I am an artist and programmer who creates digital art and interactive installations. I use code to create art that explores the relationship between humans and technology. My work often deals with themes of surveillance, control, and power. I am interested in how technology can be used to create new ways of seeing and experiencing the world. I have a background in computer science and art, and I use this knowledge to create art that is both visually stunning and intellectually stimulating.</p>
+                <p>I studied Creative Technologies at the Filmuniversity Babelsberg. The rest is history!</p>
+                <h3>📧 Get in Touch: email@sunny.com </h3>
+            </div>
+        </main>
+    );
 }
 
 export default About;
