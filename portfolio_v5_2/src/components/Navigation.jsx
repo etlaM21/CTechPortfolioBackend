a487a5b6b382c0e6f69df5ce7d3381b08b3b2f01
Malte Hillebrand <maltehillebrand96@gmail.com>
2025-02-03 18:28:27 +0100

    added site title and changed nav a bit

--- portfolio_v5_2/src/components/Navigation.jsx
+++ portfolio_v5_2/src/components/Navigation.jsx
@@ -15,10 +15,11 @@ const Navigation = () => {
 
     return (
         <div className="header" ref={headerRef} style={{borderImageSource: `linear-gradient(90deg, rgba(0, 145, 255, 0) 0%, rgba(103,184,240,1) ${mousePos.left * 100 - 24 + "%"}, rgba(128,155,252,1) ${mousePos.left * 100 + "%"}, rgba(195, 56, 210) ${mousePos.left * 100 + 16 + "%"}, rgba(2,0,36,0) 100%)`}}>
+        <h1>Sunny's Portfolio</h1>
         <nav>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
+                <li><Link to="/">🏠 Home</Link></li>
+                <li><Link to="/about">🧑 About</Link></li>
             </ul>
         </nav>
         </div>
