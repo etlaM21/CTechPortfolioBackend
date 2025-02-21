f872f2c81ceef5b870e2bd9114cca81ce3c26c8c
Malte Hillebrand <maltehillebrand96@gmail.com>
2025-02-21 11:27:53 +0100

    fixed prisma client issue when deploying

--- portfolio_v5_2/vite.config.js
+++ portfolio_v5_2/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+        '.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js',
+    },
+  },
 })
